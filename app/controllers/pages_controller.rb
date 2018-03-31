6699c79faa179561797eeca795dd74ec3bbeeb37
ElviraKutincic <elvira.kutincic@gmail.com>
2018-03-31 21:32:56 +0200

    Contact i Navbar
    Mijenjala navbar, povezala stranice ( linkove ) i na kontakt stranicu dodala kod.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,6 @@ class PagesController < ApplicationController
   def profile
   end
 
-  def explore
+  def kontakt
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get '/profile'=>'pages#profile'
 
-  get '/explore'=>'pages#explore'
+  get '/kontakt'=>'pages#kontakt'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
